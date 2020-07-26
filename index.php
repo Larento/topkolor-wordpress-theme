3a02603a2bb49070b84200859dec55cd127e1a4c
Larik <turolretar@gmail.com>
2020-07-26 20:31:13 +0300

    okjn

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<?php get_header() ?>
-<div class="tk-wrapper">
-  <h1>This is a test of Index.php page</h1>
-</div>
-<div class="tk-wrapper">
-  <h1>Hello there</h1>
-</div>
-<?php get_footer() ?>
+<?php get_header(); ?>
+<main role="main">
+  
+  <div class="tk-wrapper">
+    <h1>This is a test of Index.php page</h1>
+  </div>
+
+  <div class="tk-wrapper">
+    <h1>Hello there</h1>
+  </div>
+
+</main>
+<?php get_footer(); ?>
