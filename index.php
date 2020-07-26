02f8b7aa57cfaabcea0c88ee0c0c44b6ade303b4
Larik <turolretar@gmail.com>
2020-07-26 20:26:12 +0300

    hjgv

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<DOCTYPE !html>
-<html>
-  <?php get_header() ?>
-  <div class="tk-wrapper">
-    <h1>This is a test of Index.php page</h1>
-  </div>
-  <div class="tk-wrapper">
-    <h1>Hello there</h1>
-  </div>
-  <?php get_footer() ?>
-</html>
+<?php get_header() ?>
+<div class="tk-wrapper">
+  <h1>This is a test of Index.php page</h1>
+</div>
+<div class="tk-wrapper">
+  <h1>Hello there</h1>
+</div>
+<?php get_footer() ?>
