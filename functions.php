7a8850688b8dd411d1e9fa5582fae357bcce3886
Larik <turolretar@gmail.com>
2020-07-27 21:48:09 +0300

    Updated theme

--- functions.php
+++ functions.php
@@ -17,7 +17,8 @@
         };
         $attachments = wp_rml_get_attachments( $pictureFolder->getId() );
         foreach ( $attachments as $attachment ) {
-            wp_get_attachment_image($attachment, 'full');
+            echo "<script>console.log( $attachment )</script>"
+            //wp_get_attachment_image($attachment, 'full');
         };
 
 
