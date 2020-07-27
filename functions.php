ffccdf795a47caa9e917035f7530ed74c45ee41e
Larik <turolretar@gmail.com>
2020-07-27 20:37:54 +0300

    Updated theme

--- functions.php
+++ functions.php
@@ -5,6 +5,12 @@
 
     function tk_home_slideshow() {
         $parent = _wp_rml_root();
+        $folders = wp_rml_root_childs();
+        foreach($folders as $folder) {
+            ?>
+            <p><?php echo $folder.getId(); ?></p>
+            <?php
+        };
         $options = wp_rml_dropdown($parent, array());
         ?>
         <select style="width:100%!important;">><?php echo $options; ?></select>
