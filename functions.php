a85179458b68f730e3fe99143eb4863fdfeee861
Larik <turolretar@gmail.com>
2020-07-27 21:01:33 +0300

    Updated theme

--- functions.php
+++ functions.php
@@ -6,13 +6,18 @@
     function tk_home_slideshow() {
         $parent = _wp_rml_root();
         $folders = wp_rml_objects();
+        $pictureFolder = wp_rml_get_object_by_id($parent);
         foreach($folders as $folder) {
-            if (is_rml_folder($folder) === true) {
-                ?>
-                <p><?php echo $folder->getName(); ?></p>
-                <?php
+            if ( is_rml_folder($folder) === true ) {
+                if ( $folder->getName() === the_title() ) {
+                    $pictureFolder = $folder;
+                    break;
+                };
             };
         };
+        ?>
+        <p><?php echo $pictureFolder; ?></p>
+        <?php
         $options = wp_rml_dropdown($parent, array());
         ?>
         <select style="width:100%!important;">><?php echo $options; ?></select>
