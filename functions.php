f3bc4ea3e957efa2191dfba0d033965df161a919
Larik <turolretar@gmail.com>
2020-07-27 15:16:35 +0300

    Updated theme

--- functions.php
+++ functions.php
@@ -2,5 +2,20 @@
     function tk_scripts() {
         wp_enqueue_style('style.css', get_stylesheet_directory_uri() . '/style.css');
     };
+
+    function tk_home_slideshow() {
+        $images = get_field('home_slideshow');
+        $size = 'full'; // (thumbnail, medium, large, full or custom size)
+        if( $images ): ?>
+            <ul>
+            <?php foreach( $images as $image_id ): ?>
+                <li>
+                    <?php echo wp_get_attachment_image( $image_id, $size ); ?>
+                </li>
+            <?php endforeach; ?>
+            </ul> <?php
+        endif;
+    }
+
     add_action( 'wp_enqueue_scripts', "tk_scripts" );
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <main role="main">
-        <section class="tk-section tk-hero-section">
-            <h1>This is coole</h1>
+        <section class="tk-section tk-section-hero__button">
+            <h1>Превращаем камень в искусство</h1>
+            <?php tk_home_slideshow(); ?>
+            <button class="tk-section"></button>
         </section>
 </main>
 <?php get_footer(); ?>
