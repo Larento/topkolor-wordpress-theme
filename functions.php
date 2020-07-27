a5910dac07a9d43ec5af7aec6598b5a8e7615098
Larik <turolretar@gmail.com>
2020-07-27 21:31:51 +0300

    Updated theme

--- functions.php
+++ functions.php
@@ -1,27 +1,30 @@
 <?php
     function tk_scripts() {
-        wp_enqueue_style('style.css', get_stylesheet_directory_uri() . '/style.css');
+        wp_enqueue_style( 'style.css', get_stylesheet_directory_uri() . '/style.css' );
     };
 
     function tk_home_slideshow() {
         $parent = _wp_rml_root();
         $folders = wp_rml_objects();
-        $pictureFolder = wp_rml_get_object_by_id($parent);
+        $pictureFolder = wp_rml_get_object_by_id( $parent );
         foreach ( $folders as $folder ) {
-            if ( is_rml_folder($folder) === true ) {
+            if ( is_rml_folder( $folder ) === true ) {
                 if ( $folder->getName() === get_the_title() ) {
                     $pictureFolder = $folder;
                     break;
                 };
             };
         };
-        ?>
+        $attachments = wp_rml_get_attachments( $pictureFolder->getId() );
+        echo "<script>alert('$attachments')</script>"
+
+        /*?>
         <p><?php echo $pictureFolder->getName(); ?></p>
         <?php
         $options = wp_rml_dropdown($parent, array());
         ?>
         <select style="width:100%!important;">><?php echo $options; ?></select>
-        <?php
+        <?php*/
     };
 
     add_action( 'wp_enqueue_scripts', "tk_scripts" );
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <main role="main">
         <section class="tk-section hero">
             <h1>Превращаем камень в искусство</h1>
-            <?php the_title('<h1>', '</h1>'); ?>
             <?php tk_home_slideshow(); ?>
             <?php the_content(); ?>
             <button class="tk-section">
