0947fdc0359ac6d2cf43b9cae619ffc3da132e83
Larik <turolretar@gmail.com>
2020-07-27 19:05:41 +0300

    Updated theme

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <main role="main">
         <section class="tk-section hero">
             <h1>Превращаем камень в искусство</h1>
-            <?php tk_home_slideshow(); ?>
+            <?php wp_rml_dropdown("All Files", [], true); ?>
             <?php tk_cool_name(); ?>
             <button class="tk-section">
               <span>Cool</span>
