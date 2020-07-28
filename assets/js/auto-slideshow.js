361e4e31a0de625ba27825272affccec50810173
Larik <turolretar@gmail.com>
2020-07-28 18:49:44 +0300

    Updated theme

--- assets/js/auto-slideshow.js
+++ assets/js/auto-slideshow.js
@@ -3,6 +3,7 @@ function setSlidesCount() {
     let slideTime = parseFloat(window.getComputedStyle(slider).getPropertyValue('--slide-time'), 10);
     let slides = [...document.getElementsByClassName('slide')];
     let slidesCount = slides.length;
+    slider.style.setProperty('--slides-count', `${slidesCount}`);
     for (var i = 1; i <= slidesCount; ++i) {
         let slideNthChild = document.querySelector(`.slide:nth-child(${i})`);
         slideNthChild.style.setProperty('animation-delay', `calc(var(--slide-time) * calc(${i} - 1))`);
