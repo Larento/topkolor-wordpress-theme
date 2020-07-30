db023f1267b81e295b2f929b65e3acbb190275e2
Larik <turolretar@gmail.com>
2020-07-30 18:45:17 +0300

    Updated Theme

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
                     <li class="menu-item title"></li>
                     <?php tk_get_menu( 'Главное меню' ); ?>
                     <li class="menu-item login">
-                        <a href="/terrazzo" <?= tk_fa5_icon(007) ?>></a>
+                        <a href="/terrazzo" <?= tk_icon(007) ?>></a>
                     </li>
                     <li class="menu-item search">
                         <a href="/about" data-icon='&#xf007;'></a>
