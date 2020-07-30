be1dcb6b16797b5606dc3fd6ad0ac4d074104aee
Larik <turolretar@gmail.com>
2020-07-30 13:15:43 +0300

    Updated Theme

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <header class="tk-section header">
         <nav>
             <div>
-                <?php wp_nav_menu( array( 'menu_id' => false, 'menu_class' => 'tk-nav-menu main-menu', 'container' => false) ); ?>
+                <?php wp_nav_menu( array( 'menu_class' => 'tk-nav-menu main-menu', 'menu_id' => '', 'item-wrap' => '<ul class="%2$s">%3$s</ul>', 'container' => false) ); ?>
             </div>
         </nav>
     </header>
