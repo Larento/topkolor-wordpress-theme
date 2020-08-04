7e7ae8c07c7a3261762357d458dd0e5baf4f4290
Larik <turolretar@gmail.com>
2020-08-04 13:15:52 +0300

    Updated Theme

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
+
 <main role="main">
 	<section class="tk-section hero">
 		<?php tk_home_slideshow(); ?>
 		<h1>Превращаем камень в искусство</h1>
-			<button class="tk-button">
-			</button>
+		<button class="tk-button">
+		</button>
 	</section>
 	<section class="tk-section commerce">
-	<?php the_content(); ?>
+		<?php the_content(); ?>
 	</section>
 </main>
+
 <?php get_footer(); ?>
