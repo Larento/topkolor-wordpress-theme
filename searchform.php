b527058609e2f888da2d445237c397e5ce731a71
Larik <turolretar@gmail.com>
2020-08-04 15:16:58 +0300

    Updated Theme

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form class="search-bar" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <input type="text" class="search-field" name="s" placeholder="Search" value="<?php echo get_search_query(); ?>">
-    <input class="search-button" type="submit" value=<?php fa_icon_unicode('007') ?>>
+    <input class="search-button" type="submit" value=<?php echo fa_icon_unicode('007') ?>>
 </form>
