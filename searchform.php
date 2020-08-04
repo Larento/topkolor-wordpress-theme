25b93c9862abfc100f136804bc53514219cea3ca
Larik <turolretar@gmail.com>
2020-08-04 14:54:52 +0300

    Updated Theme

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form class="searchbar" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form class="search-bar" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <input type="text" class="search-field" name="s" placeholder="Search" value="<?php echo get_search_query(); ?>">
-    <button type="submit" value="Search">
+    <button class="search-button" type="submit" value="Search">
       <a <?= tk_icon('007', 'solid') ?>></a>
     </button>
 </form>
