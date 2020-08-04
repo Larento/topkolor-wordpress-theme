6c2c60815b6a76ea0e25eeb47dc8a2333dfd2943
Larik <turolretar@gmail.com>
2020-08-04 15:38:14 +0300

    Updated Theme

--- searchform.php
+++ searchform.php
@@ -1,12 +1,14 @@
-<form class="search-bar" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <button class="search-button" type="submit">
-    <a <?= tk_icon('002', 'solid') ?>></a>
-  </button>    
-  <input type="text" class="search-field" name="s" placeholder="Поиск по сайту" value="<?php echo get_search_query(); ?>">
-  <button class="search-button" type="submit">
-    <a <?= tk_icon('002', 'solid') ?>></a>
-  </button>   
-</form>
-<button class="close-search-button">
-  <a <?= tk_icon('002', 'solid') ?>></a>
-</button>
+<div class="search-bar">  
+  <form class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <button class="search-button-submit" type="submit">
+      <a <?= tk_icon('002', 'solid') ?>></a>
+    </button>    
+    <input type="text" class="search-field" name="s" placeholder="Поиск по сайту" value="<?php echo get_search_query(); ?>">
+    <button class="search-button-empty">
+      <a <?= tk_icon('057', 'solid') ?>></a>
+    </button>   
+  </form>
+  <button class="search-button-close">
+    <a <?= tk_icon('00d', 'solid') ?>></a>
+  </button>
+</div>
