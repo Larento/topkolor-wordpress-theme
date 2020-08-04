c9415883af8847ceb3df7f00f37de469e55c2150
Larik <turolretar@gmail.com>
2020-08-04 17:13:58 +0300

    Updated Theme

--- assets/js/main-navigation-searchbar.js
+++ assets/js/main-navigation-searchbar.js
@@ -1,11 +1,19 @@
 function hideMenuItems() {
   let searchOpenButton = document.querySelector('.header-main-navigation .search a');
   let mainMenuItems = document.querySelectorAll('.header-main-navigation .main-menu>.menu-item');
+  let searchBar = document.getElementsByClassName('search-bar')[0];
+  let searchCloseButton = document.getElementsByClassName('search-button-close')[0];
   searchOpenButton.addEventListener("click", function() {
     mainMenuItems.forEach((item) => {
-      item.classList.toggle('hidden');
+      item.classList.toggle('hidden', 1);
     });
-    
+    searchBar.classList.toggle('shown', 1);
+  });
+  searchCloseButton.addEventListener('click', function() {
+    searchBar.classList.toggle('shown', 0);
+    mainMenuItems.forEach((item) => {
+      item.classList.toggle('hidden', 0);
+    });
   });
 };
 
