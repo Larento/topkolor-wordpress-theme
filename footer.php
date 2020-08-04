992bdc6ef24ac4cc5d29a3a10d5cc16a6532bfbd
Larik <turolretar@gmail.com>
2020-08-04 20:04:21 +0300

    Updated Theme

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
 	<?php wp_footer(); ?>
+	<footer class="tk-section footer">
+		<nav class="footer-links-navigation">
+			<div class="menu-container">
+
+			</div>
+		</nav>
+		<nav class="footer-privacy-navigation">
+			<div class="menu-container">
+				
+			</div>
+		</nav>
+		<nav class="footer-contact-navigation">
+			<div class="menu-container">
+				
+			</div>
+		</nav>
+		<nav class="footer-social-navigation">
+			<div class="menu-container">
+				
+			</div>
+		</nav>
+	</footer>
 </body>
 </html>
