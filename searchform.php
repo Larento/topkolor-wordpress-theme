edbdc1153ac20ef8f3aa08771802ebb4a40dec42
Larik <turolretar@gmail.com>
2020-08-04 17:45:46 +0300

    Updated Theme

--- searchform.php
+++ searchform.php
@@ -4,11 +4,11 @@
       <a <?= tk_icon('002', 'solid') ?>></a>
     </button>    
     <input type="text" class="search-field" name="s" placeholder="Поиск по сайту" value="<?php echo get_search_query(); ?>">
-    <button class="search-button-empty" type="reset">
+    <button class="search-button-empty">
       <a <?= tk_icon('057', 'solid') ?>></a>
     </button>   
   </form>
-  <button class="search-button-close" type="reset">
+  <button class="search-button-close">
     <a <?= tk_icon('00d', 'solid') ?>></a>
   </button>
 </div>
