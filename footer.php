4fc541645cf38a7aa0257fbb18205023d820f7e7
Larik <turolretar@gmail.com>
2020-08-05 12:59:28 +0300

    Updated Theme

--- footer.php
+++ footer.php
@@ -3,19 +3,29 @@
 		<div class="container">
 			<nav class="footer-links-navigation">
 				<h3>Полезные ссылки</h3>
-				<?php tk_get_menu( 'Footer Menu Links' ); ?>
+				<ul class="main-menu">
+					<?php tk_get_menu( 'Footer Menu Links' ); ?>
+				</ul>
 			</nav>
 			<nav class="footer-privacy-navigation">
 				<h3>Информация</h3>
+				<ul class="main-menu">
 				<?php tk_get_menu( 'Footer Menu Privacy' ); ?>
+				</ul>
 			</nav>
 			<nav class="footer-contact-navigation">
 				<h3>Как связаться</h3>
-				<?php tk_get_menu( 'Footer Menu Contact' ); ?>
+				<ul class="main-menu">
+					<?php tk_get_menu( 'Footer Menu Contact' ); ?>
+				</ul>
 			</nav>
 			<nav class="footer-social-navigation">
 				<h3>Подпишитесь на нас</h3>
-				<a href="https://www.instagram.com/_tkolor/" <?= tk_icon('16d', 'brand') ?>></a>
+				<ul class="main-menu">
+					<li class="menu-item">
+					<a href="https://www.instagram.com/_tkolor/" <?= tk_icon('16d', 'brand') ?>></a>
+					</li>
+				</ul>
 			</nav>
 		</div>
 	</footer>
