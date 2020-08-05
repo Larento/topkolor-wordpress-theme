91645081b56c6e933a54d306d58a714c42b95d72
Larik <turolretar@gmail.com>
2020-08-05 10:13:05 +0300

    Updated Theme

--- assets/js/background-colors.js
+++ assets/js/background-colors.js
@@ -1,5 +1,7 @@
+function backgroundColors(bgSVG)
 var fill = window.getComputedStyle(document.documentElement).getPropertyValue('--background-fill');
 var accent = window.getComputedStyle(document.documentElement).getPropertyValue('--background-accent');
 var stroke = window.getComputedStyle(document.documentElement).getPropertyValue('--background-stroke');
+console.log(bgSVG);
 //var bgURL = read svg
-document.documentElement.style.setProperty('--background-ornament', bgURL);
+//document.documentElement.style.setProperty('--background-ornament', bgURL);
