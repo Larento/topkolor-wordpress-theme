e4fd8dea09dc1cc7039cedcdc9081571303078c0
Larik <turolretar@gmail.com>
2020-08-05 23:47:38 +0300

    Updated Theme

--- front-page.php
+++ front-page.php
@@ -5,20 +5,21 @@
 		<?php tk_home_slideshow(); ?>
 		<h1>Превращаем камень в искусство</h1>
 		<button class="tk-button" href="/terrazzo">Изделия терраццо</button>
-		<a role="button" href="/micro-concrete">Покрытия из микробетона</a>
-		<a role="button" href="/decorative-concrete">Продукция из арт-бетона</a>
+		<button class="tk-button" href="/micro-concrete">Покрытия из микробетона</button>
+		<button class="tk-button" href="/decorative-concrete">Продукция из арт-бетона</button>
 	</section>
 	<section class="tk-section commerce">
 		<h2>Почему ТОПКОЛОР?</h2>
 		<div class="list-wrapper">
-			<h3>Оптимальный уровень цены при высоком качестве изделий</h3>
 			<a <?= tk_icon('007', 'solid') ?>></a>
+			<h3>Оптимальный уровень цены при высоком качестве изделий</h3>
 			<ul class="advantages-list">
 				<li>Качество нашей продукции определяется её <strong>долговечностью</strong>, <strong>надежностью</strong> и <strong>функциональностью</strong></li>
 				<li>На каждое изделие предоставляется <strong>гарантия на 12 месяцев</strong></li>
 			</ul>
 		</div>
 		<div class="list-wrapper">
+			<a <?= tk_icon('007', 'solid') ?>></a>
 			<h3>Полный цикл изготовления изделий</h3>
 			<ul class="advantages-list">
 				<li>Проектирование 3D модели</li>
@@ -30,6 +31,7 @@
 			</ul>
 		</div>
 		<div class="list-wrapper">
+			<a <?= tk_icon('007', 'solid') ?>></a>
 			<h3>Собственное производство на основе оригинальных технологий</h3>
 			<ul class="advantages-list">
 				<li><strong>Сокращение срока</strong> изготовления изделий</li>
@@ -37,13 +39,14 @@
 			</ul>	
 		</div>
 		<div class="list-wrapper">
-		<h3>Сотрудничество с ведущими архитектурными бюро</h3>
+			<a <?= tk_icon('007', 'solid') ?>></a>
+			<h3>Сотрудничество с ведущими архитектурными бюро</h3>
 			<ul class="advantages-list">
 				<li>Архитектурное бюро «САГА» (г. Москва)</li>
 				<li>Артбюро «1/1»</li>
 			</ul>	
 		</div>
-		<button href="/about">Подробнее о компании</button>
+		<button class="tk-button" href="/about">Подробнее о компании</button>
 	</section>
 </main>
 
