3571fd4c702386e0c94d75643fd920d69c352d71
Larik <turolretar@gmail.com>
2020-08-12 12:13:28 +0300

    Updated Theme

--- assets/js/home-slideshow.js
+++ assets/js/home-slideshow.js
@@ -1,20 +1,21 @@
+function cycle(current, length) {
+  return (current % length);
+};
+
 function homepageSlideshow() {
-  var homeSwiper = new Swiper('.tk-slider', {
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true,
-    },
-    autoplay: {
-      delay: 5000,
-    },
-    autoplayDisableOnInteraction: false,
-    slidesPerView: 1,
-    lazy: true,
-    lazy: {
-      loadPrevNext: true,
-    },
-    preloadImages: false,
-  });
+  let initialSlide = 1;
+  let duration = 5000;
+  let slider = document.querySelector('.tk-slider.homepage');
+  let slides = slider.querySelectorAll('.slide');
+  var i = initialSlide - 1;
+  slides[i].classList.toggle('shown', 1);
+  setInterval(function() {
+      console.log(i);
+      slides[cycle(i, slides.length)].classList.toggle('shown', 0);
+      i = cycle(i + 1, slides.length);
+      console.log(i);
+      slides[cycle(i, slides.length)].classList.toggle('shown', 1);
+  }, duration);
 };
 
 document.addEventListener("DOMContentLoaded", homepageSlideshow);
