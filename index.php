e20e0dd24792b0d34ce88b8053dd899fd0bec4ef
Larik <turolretar@gmail.com>
2020-08-13 20:39:48 +0300

    Updated Theme

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 		<div class="container">
 		<?php the_title( '<h3>', '</h3>' ); ?>
 		<?php the_content(); ?>
-		<code> <?php echo get_terms( array(
-    'taxonomy' => 'post_tag',
-    'hide_empty' => false,
-) )[0]->name;/*get_the_terms(get_the_ID(), 'product_style')->name;*/ ?> </code>
+		<code> <?php
+				$terms = get_the_terms(get_the_ID(), 'product_style');
+				foreach ( $terms as $term )
+				echo $term->slug; 
+		?> </code>
 		
 		</div>
 	</section>
