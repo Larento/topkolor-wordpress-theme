6eaa97dda9e8e712ebfc0cd961433ae917b3920e
Larik <turolretar@gmail.com>
2020-08-13 19:55:50 +0300

    Updated Theme

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<main role="main">
+	<section class="tk-section post document">
+		<div class="container">
+    <h3>Ошибка 404</h3>
+    <p>Страница не найдена!</p>
+		</div>
+	</section>
+</main>
+<?php get_footer(); ?>
