b0745d2fdb2ded2170195f25af02383d5d269a71
Larik <turolretar@gmail.com>
2020-08-13 11:25:24 +0300

    Updated Theme

--- functions.php
+++ functions.php
@@ -131,4 +131,19 @@
 		?> </div> <?php
 	};
 
+	function wporg_custom_post_type() {
+    register_post_type('wporg_product',
+			array(
+				'labels' => array(
+					'name'          => __('Products', 'textdomain'),
+					'singular_name' => __('Product', 'textdomain'),
+				),
+					'public'      => true,
+					'has_archive' => true,
+			),
+    );
+	};
+
+	add_action('init', 'wporg_custom_post_type');
+
 ?>
