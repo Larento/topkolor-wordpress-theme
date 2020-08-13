6e92fdac9d749531c8e3caf808ed01f95bf699fc
Larik <turolretar@gmail.com>
2020-08-13 20:29:42 +0300

    Updated Theme

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 		<div class="container">
 		<?php the_title( '<h3>', '</h3>' ); ?>
 		<?php the_content(); ?>
-		<code> <?php echo taxonomy_exists( 'product_kind' );/*get_the_terms(get_the_ID(), 'product_style')->name;*/ ?> </code>
+		<code> <?php echo get_terms( array(
+    'taxonomy' => 'post_tag',
+    'hide_empty' => false,
+) );/*get_the_terms(get_the_ID(), 'product_style')->name;*/ ?> </code>
 		
 		</div>
 	</section>
