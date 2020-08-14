d8f30adcdbe4f13db41390b24e3c24835f4a4091
Larik <turolretar@gmail.com>
2020-08-14 10:27:26 +0300

    Updated Theme

---
+++ taxonomy-product_style.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<main role="main">
+	<section class="tk-section post document">
+		<div class="container">
+    <h3>Эта архив для product_style</h3>
+		</div>
+	</section>
+</main>
+<?php get_footer(); ?>
