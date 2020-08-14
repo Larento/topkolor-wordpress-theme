b263ac9ee32bee0b087f2fadc0ca5967ed329dcf
Larik <turolretar@gmail.com>
2020-08-14 22:50:55 +0300

    Updated Theme

--- search.php
+++ search.php
@@ -5,6 +5,23 @@
     <h3>Поиск</h3>
     <?php get_search_form(); ?>
 		</div>
-	</section>
+  </section>
+  <?php
+  if ( have_posts() ) {
+    while ( have_posts() ) {
+      ?>
+	    <section class="tk-section post document">
+		    <div class="container">
+          <?php
+          the_post(); 
+          the_title( '<h3>', '</h3>' );
+          the_content();
+          ?>
+		    </div>
+      </section>
+      <?php
+    };
+  };
+  ?>
 </main>
 <?php get_footer(); ?>
