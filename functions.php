1c9a1baf005c72e5a102dd522f3179a51bca8c3f
Larik <turolretar@gmail.com>
2020-08-14 23:22:03 +0300

    Updated Theme

--- functions.php
+++ functions.php
@@ -75,7 +75,7 @@
 
 	function new_excerpt_more($more) {
 		global $post;
-	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Читать далее...</a>';
+	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Подробнее...</a>';
 	};
 
 	add_filter('excerpt_more', 'new_excerpt_more');
