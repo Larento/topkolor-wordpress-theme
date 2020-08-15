763d9e9373dfd8c5df21d707e6049d3989205267
Larik <turolretar@gmail.com>
2020-08-15 13:26:51 +0300

    Updated Theme

--- page-request.php
+++ page-request.php
@@ -4,24 +4,39 @@
 		<div class="container">
 		<?php the_title( '<h3>', '</h3>' ); ?>
     <h4>There will be a form here</h4>
-    <form>
+    <form action="/request/success">
       <div class="style">
-        <p>Стиль</p>
+        <p>Стиль изделия:</p>
         <?php
-        $post_types = get_post_types( ['description'  => 'Product',], 'objects' );
-        foreach ( $post_types as $post_type ) {
-          $slug = $post_type->name;
-          $label = $post_type->labels->all_items;
-          ?>
-            <input type="radio" id=<?= $slug ?> name="style" value=<?= $slug ?>>
-            <label for=<?= $slug ?>><?= $label ?></label><br>
-          <?php
-        };
+          $post_types = get_post_types( ['description'  => 'Product',], 'objects' );
+          $taxonomy_slug = [];
+          foreach ( $post_types as $post_type ) {
+            $post_slug = $post_type->name;
+            $post_label = $post_type->labels->all_items;
+            $taxonomy_slug[$post_slug] = tk_taxonomy_name('', $post_slug);
+            ?>
+              <input type="radio" id=<?= $post_slug ?> name="style" value=<?= $post_slug ?>>
+              <label for=<?= $post_slug ?>><?= $post_label ?></label><br>
+            <?php
+          };
         ?>
       </div>
 
       <div class="kind">
-        <p>Please select your age:</p>
+        <p>Вид изделия:</p>
+        <?php
+          foreach ($post_types as $post_slug => $post_type->name) {
+            $taxonomy_terms = get_terms($taxonomy_slug[$post_slug]);
+            foreach ($taxonomy_terms as $term) {
+              ?>
+              <span><?= $term ?></span>
+              <?php
+            };
+          };
+        ?>
+
+
+
         <input type="radio" id="age1" name="age" value="30">
         <label for="age1">0 - 30</label><br>
         <input type="radio" id="age2" name="age" value="60">
