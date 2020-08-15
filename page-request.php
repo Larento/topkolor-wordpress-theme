8a55a6d30611ce1e69841f0cd024a4e6cdd20368
Larik <turolretar@gmail.com>
2020-08-15 10:56:57 +0300

    Updated Theme

---
+++ page-request.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<main role="main">
+	<section class="tk-section post document">
+		<div class="container">
+		<?php the_title( '<h3>', '</h3>' ); ?>
+		<h4>There will be a form here</h4>
+		</div>
+	</section>
+</main>
+<?php get_footer(); ?>
