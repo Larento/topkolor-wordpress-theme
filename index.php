e8aa4f966f0a06e957b6a5d20a351a23af84bc25
Larik <turolretar@gmail.com>
2020-08-17 22:46:50 +0300

    Updated Theme

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 		<?php
 			$product = tk_get_current_product();
 			$bruh = tk_get_current_product_kind($product);
+			$bruh = tk_get_product_slug($product);
 			echo var_dump($bruh); 
 		?>
 		</div>
