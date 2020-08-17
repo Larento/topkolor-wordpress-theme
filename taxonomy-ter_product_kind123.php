d539808d196ca10db6ed79edddfedcb3d5249dcb
Larik <turolretar@gmail.com>
2020-08-17 23:15:21 +0300

    Updated Theme

--- taxonomy-ter_product_kind123.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<main role="main">
-	<section class="tk-section post document">
-		<div class="container">
-    <h3>Эт архив для product_style</h3>
-		</div>
-	</section>
-</main>
-<?php get_footer(); ?>
