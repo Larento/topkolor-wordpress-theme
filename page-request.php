6b9af53b501546e9d2a1cdcf8958afb46e675032
Larik <turolretar@gmail.com>
2020-08-18 11:09:45 +0300

    Updated Theme

--- page-request.php
+++ page-request.php
@@ -5,14 +5,37 @@
       <?php the_title( '<h3>', '</h3>' ); ?>
       <h4>There will be a form here</h4>
       <form class="request-form" action="/request?success=true">
+        <?php
+          $style = $_GET['style'];
+          $kind = $_GET['kind'];
+          $products = tk_get_products();
+          $product_kinds = tk_get_product_kinds();
+          $style_is_valid = false;
+          $kind_is_valid = false;
+          foreach ( $products as $product ) {
+            if ( $style === tk_get_product_slug($product) ) {
+              $style_is_valid = true;
+            };
+          };
+          if ( $style_is_valid === false ) {
+            $style = tk_get_product_slug(reset($products));
+          };
+          foreach ( $product_kinds as $product_kind ) {
+            if ( $kind === tk_get_product_kind_slug($product_kind) ) {
+              $kind_is_valid = true;
+            };
+          };
+          if ( $kind_is_valid === false ) {
+            $kind = tk_get_product_kind_slug(reset($product_kinds));
+          };
+        ?>
         <div class="style">
           <p>Стиль изделия:</p>
           <?php
-            $products = tk_get_products();
             foreach ( $products as $product ) {
               $product_slug = tk_get_product_slug($product);
               ?>
-                <input type="radio" id=<?= $product_slug ?> name="style" value=<?= $product_slug ?>>
+                <input type="radio" id=<?= $product_slug ?> name="style" value=<?= $product_slug ?> <?php if ($product_slug === $style) {echo "checked='checked'";}; ?>>
                 <label for=<?= $product_slug ?>><?= tk_get_product_label($product) ?></label><br>
               <?php
             };
@@ -39,6 +62,7 @@
             };
           ?>
         </div>
+        
       </form>
 		</div>
 	</section>
