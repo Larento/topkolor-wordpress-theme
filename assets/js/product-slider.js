187ee9236211694b52b02035dc76fc9ad3008ab2
Larik <turolretar@gmail.com>
2020-08-22 01:04:23 +0300

    Updated Theme

--- assets/js/product-slider.js
+++ assets/js/product-slider.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+  Flickity.prototype._touchActionValue = 'pan-y pinch-zoom';
   let slider = document.querySelectorAll('.tk-slider.product');
   var flickSlider = new Flickity( '.tk-slider.product', {
     setGallerySize: false,
@@ -7,13 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   let fullscreenImages = document.querySelectorAll('.is-fulscreen .slide img');
     // assign wheelzoom
-  wheelzoom(fullscreenImages);
+  //wheelzoom(fullscreenImages);
 
   // assign and set the zoom increment percentage
-  wheelzoom(fullscreenImages, {zoom: 0.05});
+  //wheelzoom(fullscreenImages, {zoom: 0.05});
 
   // set a maximum zoom amount
-  wheelzoom(fullscreenImages, {maxZoom: 2});
+  //wheelzoom(fullscreenImages, {maxZoom: 2});
 
   // zooming out can be triggered by calling 'wheelzoom.reset'
   //document.querySelector('img').dispatchEvent(new CustomEvent('wheelzoom.reset'));
