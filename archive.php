b1045f3ea183c6554382810505d9a6bdb38e1b27
Larik <turolretar@gmail.com>
2020-08-21 12:51:00 +0300

    Updated Theme

--- archive.php
+++ archive.php
@@ -1,10 +1,5 @@
 <?php get_header(); ?>
 <main role="main">
-  <section class="tk-section post document">
-    <div class="container">
-      <h3>Why hello there, 'tis an archive page!</h3>
-    </div>
-  </section>
   <?php
   if ( have_posts() ) {
     while ( have_posts() ) {
