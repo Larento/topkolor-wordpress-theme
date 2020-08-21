99706b3ca2236b129a8efb5b968328bcf8a35d05
Larik <turolretar@gmail.com>
2020-08-21 17:19:04 +0300

    Updated Theme

--- single.php
+++ single.php
@@ -3,14 +3,14 @@
 		<div class="container">
     <?php
       $attachments = tk_get_product_media();
-      if ( strpos($attachments, 'Error') === false ) {
+      //if ( strpos($attachments, 'Error') === false ) {
         foreach ( $attachments as $attachment ) {
           $URL = wp_get_attachment_image_url( $attachment, 'full' );
           ?>
             <img src=<?= $URL ?> alt="kinky"> 
           <?php
-        }
-      };
+        };
+     // };
     ?>
 		<?php the_title( '<h3>', '</h3>' ); ?>
 		<?php the_content(); ?>
