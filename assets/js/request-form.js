ade06957f267c4318d22bfe604bd9e30faa9cfb2
Larik <turolretar@gmail.com>
2020-08-21 09:55:10 +0300

    Updated Theme

--- assets/js/request-form.js
+++ assets/js/request-form.js
@@ -24,28 +24,16 @@ function setForm() {
   });
   styleSelect.addEventListener("change", function() {
     console.log(this.options[this.selectedIndex]);
+    let selectedOption = this.options[this.selectedIndex];
+    Array.from(product_types[this.value]['kinds']).forEach((kind) => {
+      let selected = false;
+      if (kind['slug'] == params['kind']) {
+        selected = true;
+      };
+      let newOption = new Option(kind['label'], kind['slug'], selected, selected);
+      kindSelect.append(newOption);
+    });
   });
-  styleSelect.addEventListener("load", function() {
-    console.log(this.options[this.selectedIndex]);
-  });
-  //updateForm(styleContainer, kindContainer);
-  //Shows the right container
-  //styleRadioInputs.forEach((radio_button) => {
-  //   radio_button.addEventListener('click', function() {
-  //     updateForm(styleContainer, kindContainer);
-  //   });
-  // });
 };
 
-function updateForm(styleContainer, kindContainer) {
-  let styleRadioValue = styleContainer.querySelector("input[type='radio']:checked").value;
-  let kindRadioInputsContainers = kindContainer.querySelectorAll("div.radio-inputs");
-  kindRadioInputsContainers.forEach((container) => {
-    container.classList.toggle('shown', 0);
-    let correctContainer = kindContainer.querySelector("div.radio-inputs." + styleRadioValue);
-    if (correctContainer !== null) {
-      correctContainer.classList.toggle('shown', 1);
-    };
-  });
-};
 
