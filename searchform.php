2173b8c79595d322113ae53885ac6d18c3fe3024
Larik <turolretar@gmail.com>
2020-08-27 23:50:02 +0300

    Updated Theme

--- searchform.php
+++ searchform.php
@@ -1,3 +1,4 @@
+<?php use \tk\functions as tk; ?>
 <form class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <button class="search-button-submit" type="submit">
     <a class="no-text" <?= tk\icon('f002', 'solid') ?>></a>
