58419450347124056cda53e89b653f6ec4b8648c
Larik <turolretar@gmail.com>
2020-08-31 20:40:02 +0300

    Updated Theme

--- assets/topkolor/js/request-form.js
+++ assets/topkolor/js/request-form.js
@@ -1,12 +1,51 @@
-document.addEventListener("DOMContentLoaded", setForm);
-// let selectedKind = [];
-// Object.entries(productTypes).forEach((style) => {
-//   const [key, value] = style;
-//   selectedKind[key] = false;
-// });
+document.addEventListener("DOMContentLoaded", initForm);
+
+function initForm() {
+  let JSONParams = getFormParams();
+  params['style'], params['kind'] = 'none';
+  if ( JSONParams ) {
+    params = JSON.parse( JSONParams );
+  }
+  console.log(params);
+
+}
+
+function getFormParams() {
+  let queryString = window.location.search.substring(1);
+  let urlParams = new URLSearchParams(queryString);
+  let postID = urlParams.get('post_id');
+  return customAPIRequest( 'get_request_form_params', postID );
+}
+
+function getProducts() {
+  return customAPIRequest( 'get_products' );
+}
+
+function customAPIRequest( functionName, parameter = '' ) {
+  let hostname = window.location.hostname;
+  let protocol = window.location.protocol;
+  let restURL = `/wp-json/tk-wordpress-plugin/v1/functions/${functionName}`;
+  let fetchURL = protocol + '//' + hostname + restURL + '/' + parameter;
+  fetch(fetchURL)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          return data;
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+}
+
+
 
 function setForm() {
-  getFormParams();
   let form = document.querySelector(".request-form");
   let styleSelect = form.querySelector("select#style-select");
   let kindSelect = form.querySelector("select#kind-select");
@@ -42,27 +81,4 @@ function updateForm(styleSelect, kindSelect) {
   });
 };
 
-function getFormParams() {
-  let queryString = window.location.search.substring(1);;
-  let hostname = window.location.hostname;
-  let protocol = window.location.protocol;
-  let urlParams = new URLSearchParams(queryString);
-  let postID = urlParams.get('post_id');
-  console.log(postID);
-  let url = '/wp-json/tk-wordpress-plugin/v1/functions/get_request_form_params/';
-  fetch(protocol + '//' + hostname + url + postID)
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' + response.status);
-          return;
-        }
-        response.json().then(function(data) {
-          console.log(data);
-        });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-}
+
